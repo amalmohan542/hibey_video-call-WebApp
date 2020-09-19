0351c662d779c3ff8e380d3af450c2184a0c65f1
Amal Mohan K <amalmohan542@gmail.com>
2020-09-19 23:29:47 +0530

    Server port set

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
 const server = require('http').Server(app);
+const { v4: uuidv4 } = require('uuid');
+uuidv4();
+
+app.set('view engine','ejs');
 
 app.get('/',(req,res)=>{
-    res.status(200).write("Hello World");
-
+    res.redirect('/${uuidv4()}');
+})
+app.get('/:room',(req,res)=> {
+    res.render('room',{roomId : req.params.room })
 })
 
-server.listen("3000");
+app.listen("3000",function(){
+    console.log("Server start running on port 3000");
+});
